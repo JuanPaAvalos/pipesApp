871e723a391ec82f2ae410ad29705cda391ce7de
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-07-22 12:39:45 -0500

    feat: add Prime table, toolbar, split button, and splitter components in prime module

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-//prime Ng 
+//prime Ng
 import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
 import {MenubarModule} from 'primeng/menubar';
 import {FieldsetModule} from 'primeng/fieldset';
-
-
+import { ToolbarModule } from 'primeng/toolbar';
+import { SplitterModule } from 'primeng/splitter';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-   
+
   ],
   exports:[
     ButtonModule,
     CardModule,
     MenubarModule,
     FieldsetModule,
+    ToolbarModule,
+    SplitterModule,
+    SplitButtonModule,
+    TableModule,
   ]
 })
 export class PrimeNGModule { }
