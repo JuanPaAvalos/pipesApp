26a2d1a78e016044c77e7677fc1cc569372e397b
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-07-22 12:41:18 -0500

    feat: add heroes array

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,6 +8,31 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class OrdenarComponent implements OnInit {
+
+  public isUpperCase: boolean = false
+
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.red
+    },
+    {
+      name: 'Batman',
+      canFly: false,
+      color: Color.black
+    },
+    {
+      name: 'Green Lantern',
+      canFly: true,
+      color: Color.green
+    },
+    {
+      name: 'Flash',
+      canFly: false,
+      color: Color.red
+    },
+  ]
 
   constructor() { }
 
